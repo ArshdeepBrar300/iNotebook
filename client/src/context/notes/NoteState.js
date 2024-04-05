a6f70a2128c93f457ea81f2ee0e98c0ccac3a1f6
ArshdeepBrar300 <arshdeepbrar171@gmail.com>
2024-04-05 23:00:18 +0530

    urlFix

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = props => {
-  const host = "https://inotebookserver-db1v.onrender.com/";
+  const host = "https://inotebookserver-db1v.onrender.com";
   const notesInitial = [];
 
   const [notes, setNotes] = useState(notesInitial);
